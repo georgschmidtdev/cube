498ec0a709864b4eb726e70a69a0e729b844c076
Student Jakob Ringler <jakob.ringler@hans.hs-ansbach.de>
2020-01-24 18:21:58 +0100

    Map layout

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,42 @@
+﻿
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public GameObject ImpactEffect;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();  
+        }
+    }
+
+    void Shoot ()
+    {
+        muzzleFlash.Play();
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            { 
+                    target.TakeDamage(damage);
+            }
+
+            GameObject impactGO = Instantiate(ImpactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 0.1f);
+        }
+    }
+}
