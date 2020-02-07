adcd150d090078fe7019f0da4f09ce868dd9cad5
Student Jakob Ringler <jakob.ringler@hans.hs-ansbach.de>
2020-02-07 12:25:24 +0100

    killPlane

---
+++ Assets/Scripts/killPlane.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killPlane : MonoBehaviour
+{
+
+    public float movementSpeed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, 1, 0) * movementSpeed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+;        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("dead");
+            // DEATHSCREEN + RESPAWN + Current Time
+        }
+    }
+}
