19956cf270d5dcf442249f2d37a26ad2b0527a3f
jakobringler <58333355+jakobringler@users.noreply.github.com>
2020-02-09 01:25:19 +0100

    laser Plane + AO

--- Assets/Scripts/killPlane.cs
+++ Assets/Scripts/killPlane.cs
@@ -8,16 +8,23 @@ public class killPlane : MonoBehaviour
     public float movementSpeed = 0.1f;
     private Vector3 startPos = new Vector3(65f, -0.8f, -3f);
 
+    public Material material;
+
     // Start is called before the first frame update
     void Start()
     {
         startPos = transform.position;
+
+        material = GetComponent<Renderer>().material;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(new Vector3(0, 1, 0) * movementSpeed * Time.deltaTime);
+
+        //material.color.a = Random.Range(0.25f, 0.75f);
 
         if (Input.GetKeyDown(KeyCode.G))
         {
